a0d23d7827dfd0c972c832c1dac88cc836d1ae76
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-11-03 15:50:01 +0100

    moved light, camera

--- house.js
+++ house.js
@@ -9,21 +9,23 @@ renderer.setClearColor(0xffffff, 1);
 document.body.appendChild(renderer.domElement);
 
 // Würfel mit Phong-Oberfläche
-var geometry = new THREE.BoxGeometry(1, 1, 1);
+var geometry = new THREE.BoxGeometry(10, 10, 10);
 var material = new THREE.MeshPhongMaterial({ambient: 0x050505, color: 0x0033ff, specular: 0x555555, shininess: 30});
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 // gerichtete Lichtquelle: entspricht Sonne: parallele Lichtstrahlen unendlich weit weg in 1 Richtung
 var light = new THREE.DirectionalLight(0xffffff);
-light.position.set(2, 1, 1);
+light.position.set(10, 20, 30);
 light.target = cube;
 scene.add(light);
 
-camera.position.set(1, 1, 1);
+camera.position.set(12, 12, 12);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
+renderer.render(scene, camera);
 
+/*
 function animate() {
 	requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
@@ -32,3 +34,9 @@ function animate() {
 	renderer.render(scene, camera);
 }
 animate();
+*/
+
+
+
+
+
