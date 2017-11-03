bf1b52493b0112164926ea7205c674e038d600f4
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-11-03 16:31:12 +0100

    blubb

--- house.js
+++ house.js
@@ -8,19 +8,31 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xffffff, 1);
 document.body.appendChild(renderer.domElement);
 
-// Würfel mit Phong-Oberfläche
-var geometry = new THREE.BoxGeometry(10, 10, 10);
-var material = new THREE.MeshPhongMaterial({ambient: 0x050505, color: 0x0033ff, specular: 0x555555, shininess: 30});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// erzeuge Haus
+var houseSize = 10;
+var houseGeometry = new THREE.BoxGeometry(houseSize, houseSize, houseSize);
+var houseMaterial = new THREE.MeshPhongMaterial({ambient: 0x050505, color: 0x0033ff, specular: 0x555555, shininess: 30});
+var house = new THREE.Mesh(houseGeometry, houseMaterial);
+scene.add(house);
+
+/*
+// erzeuge Hausdach
+var radius = houseSize/2*Math.sqrt(2);    // radius ist hier Distanz Ecke-Mitte
+var height = 5;
+var roofGeometry = new THREE.CylinderGeometry(0, radius, height, 4, 1);
+var roof = new THREE.Mesh(roofGeometry, houseMaterial);
+roof.position.set(0, 10, 0);
+roof.rotation.y += Math.PI/4;
+scene.add(roof);
+*/
 
 // gerichtete Lichtquelle: entspricht Sonne: parallele Lichtstrahlen unendlich weit weg in 1 Richtung
 var light = new THREE.DirectionalLight(0xffffff);
 light.position.set(10, 20, 30);
-light.target = cube;
+light.target = house;
 scene.add(light);
 
-camera.position.set(12, 12, 12);
+camera.position.set(9, 11, 13);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 renderer.render(scene, camera);
