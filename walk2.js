a0be3e71c4f3242936b80c98e2bf0d26c0c163b0
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-11-04 16:48:49 +0100

    added axis drawer

--- walk2.js
+++ walk2.js
@@ -26,6 +26,7 @@ function createControls(camera){
     return controls;
 }
 
+
 function init() {
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
     camera.position.z = 500;
@@ -38,8 +39,9 @@ function init() {
     // TODO fog später für Wolken/Nebel reinhacken
     //scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
 
+    /*
     var geometry = new THREE.CylinderGeometry(0, 10, 30, 4, 1);
-    var material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true });
+    var material = new THREE.MeshPhongMaterial({color: 0xffffff, flatShading: true});
 
     for (var i = 0; i < 500; i ++) {
         var mesh = new THREE.Mesh(geometry, material);
@@ -50,6 +52,25 @@ function init() {
         mesh.matrixAutoUpdate = false;
         scene.add(mesh);
     }
+    */
+    
+    scene.add(buildAxes(1000));
+    
+    var houseSize = 10;
+    var houseGeometry = new THREE.BoxGeometry(houseSize, houseSize, houseSize);
+    var houseMaterial = new THREE.MeshPhongMaterial({ambient: 0x050505, color: 0x0033ff, specular: 0x555555, shininess: 30});
+    var house = new THREE.Mesh(houseGeometry, houseMaterial);
+    scene.add(house);
+
+    // erzeuge Hausdach
+    var radius = houseSize/2*Math.sqrt(2);    // radius ist hier Distanz Ecke-Mitte
+    var height = 5;
+    var roofGeometry = new THREE.CylinderGeometry(0, radius, height, 4, 1);
+    var roof = new THREE.Mesh(roofGeometry, houseMaterial);
+    roof.position.set(0, 10, 0);
+    roof.rotation.y += Math.PI/4;
+    scene.add(roof);
+    
 
     // lights
     var light = new THREE.DirectionalLight(0xffffff);
@@ -76,14 +97,12 @@ function init() {
     render();
 }
 
+// TODO kicken?
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize(window.innerWidth, window.innerHeight);
-
     controls.handleResize();
-
     render();
 }
 
