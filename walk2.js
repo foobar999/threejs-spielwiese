885f52f4215288f5c88977f55c744ac8fb835eba
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-11-04 16:35:54 +0100

    m(

--- walk2.js
+++ walk2.js
@@ -10,7 +10,7 @@ function createControls(camera){
     var controls = new THREE.TrackballControls(camera);
 
     controls.rotateSpeed = 1.0;
-    controls.zoomSpeed = 1.2;
+    controls.zoomSpeed = 10.2;
     controls.panSpeed = 0.8;
 
     controls.noZoom = false;
@@ -19,6 +19,7 @@ function createControls(camera){
     controls.staticMoving = true;
     controls.dynamicDampingFactor = 0.3;
 
+    // a, s, d
     controls.keys = [65, 83, 68];
 
     controls.addEventListener('change', render);
