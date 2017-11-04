23a6924c6df8e8ac6cdc9bd5447b4e1c9acd316b
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-11-04 15:56:02 +0100

    fixed indendation

--- walk.js
+++ walk.js
@@ -6,84 +6,68 @@ render(); // remove when using next line for animation loop (requestAnimationFra
 //animate();
 
 function init() {
-
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xcccccc );
-    scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+    scene.background = new THREE.Color(0xcccccc);
+    scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
 
     renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-    var container = document.getElementById( 'container' );
-    container.appendChild( renderer.domElement );
+    var container = document.getElementById('container');
+    container.appendChild(renderer.domElement);
 
-    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
     camera.position.z = 500;
 
-    controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.addEventListener( 'change', render ); // remove when using animation loop
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', render); // remove when using animation loop
     // enable animation loop when using damping or autorotation
     //controls.enableDamping = true;
     //controls.dampingFactor = 0.25;
     controls.enableZoom = false;
 
     // world
+    var geometry = new THREE.CylinderGeometry(0, 10, 30, 4, 1);
+    var material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true });
 
-    var geometry = new THREE.CylinderGeometry( 0, 10, 30, 4, 1 );
-    var material = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } );
-
-    for ( var i = 0; i < 500; i ++ ) {
-
-        var mesh = new THREE.Mesh( geometry, material );
-        mesh.position.x = ( Math.random() - 0.5 ) * 1000;
-        mesh.position.y = ( Math.random() - 0.5 ) * 1000;
-        mesh.position.z = ( Math.random() - 0.5 ) * 1000;
+    for (var i = 0; i < 500; i ++) {
+        var mesh = new THREE.Mesh(geometry, material);
+        mesh.position.x = (Math.random() - 0.5) * 1000;
+        mesh.position.y = (Math.random() - 0.5) * 1000;
+        mesh.position.z = (Math.random() - 0.5) * 1000;
         mesh.updateMatrix();
         mesh.matrixAutoUpdate = false;
-        scene.add( mesh );
-
+        scene.add(mesh);
     }
 
     // lights
+    var light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(1, 1, 1);
+    scene.add(light);
 
-    var light = new THREE.DirectionalLight( 0xffffff );
-    light.position.set( 1, 1, 1 );
-    scene.add( light );
+    var light = new THREE.DirectionalLight(0x002288);
+    light.position.set(-1, -1, -1);
+    scene.add(light);
 
-    var light = new THREE.DirectionalLight( 0x002288 );
-    light.position.set( -1, -1, -1 );
-    scene.add( light );
+    var light = new THREE.AmbientLight(0x222222);
+    scene.add(light);
 
-    var light = new THREE.AmbientLight( 0x222222 );
-    scene.add( light );
-
-
-    window.addEventListener( 'resize', onWindowResize, false );
-
+    window.addEventListener('resize', onWindowResize, false);
 }
 
 function onWindowResize() {
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
-
-    requestAnimationFrame( animate );
-
+    requestAnimationFrame(animate);
     controls.update(); // required if controls.enableDamping = true, or if controls.autoRotate = true
-
     render();
-
 }
 
 function render() {
-
-    renderer.render( scene, camera );
-
+    renderer.render(scene, camera);
 }
